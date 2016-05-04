02116f8a1cc135dda7027b2b1968d2640d43a7e2
MidnightDrifter <linkmasterchief@aol.com>
2016-05-04 14:35:46 -0700

    Function skeletons set up

--- Lab1-Matrices/matrix.c
+++ Lab1-Matrices/matrix.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define INT_SIZE sizeof(int)
+
 void  matrix_print( Matrix m, int num_rows, int num_columns ) {
     int i, j;
     for ( i=0; i<num_rows; ++i ) {
@@ -11,3 +13,43 @@ void  matrix_print( Matrix m, int num_rows, int num_columns ) {
         printf( "\n" );
     }
 }
+
+
+
+
+Matrix matrix_create(int num_rows, int num_columnss)
+{
+
+	
+
+}
+
+void matrix_add(Matrix m1, Matrix m2, Matrix result, int num_rows, int num_columns)
+{
+
+}
+
+Matrix matrix_transpose(Matrix matrix, int num_rows, int num_columns)
+{
+
+
+}
+
+void matrix_delete(Matrix matrix)
+{
+
+
+}
+
+void matrix_delete_row(Matrix matrix, int r, int num_rows)
+{
+
+}
+
+void matrix_delete_column(Matrix matrix, int c, int num_rows, int num_cols)
+{
+
+}
+
+
+
